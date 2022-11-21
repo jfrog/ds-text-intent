b7d61295153da62d1a671e161b30c33ee8b4eb81
Shon Mendelson <37176115+seanm92@users.noreply.github.com>
2022-11-21 11:13:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -101,6 +101,7 @@ def aggregate(source, days_back=1):
 
     cols = list(source_df.columns)
     fields = [x for x in cols if x not in non_text_cols]
+    source_df['ParentId'] = source_df['ParentId'].astype(str)
     for index, row in source_df.iterrows():
         account_id = None
         email_id = None
